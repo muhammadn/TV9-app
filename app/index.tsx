c8c7c5bc415d57914596a595146c0ab85514a189
Muhammad Nuzaihan Bin Kamal Luddin <muhammad@taqisystems.com>
2024-11-03 07:52:54 +0800

    save working version

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppState, BackHandler, StyleSheet } from 'react-native';
+import { View, AppState, BackHandler, StyleSheet } from 'react-native';
 import { useScale } from '@/hooks/useScale';
 import { ThemedView } from '@/components/ThemedView';
 import Video, { VideoRef } from 'react-native-video';
@@ -14,45 +14,58 @@ export default function AlHijrah() {
 
   const appState = useRef(AppState.currentState);
   const [appStateVisible, setAppStateVisible] = useState(appState.current);
+  const [isLoading, setLoading] = useState(true);
+
+  const getLink = async() => {
+    try {
+      const response = await fetch("https://streaming.unrealasia.net/tvalhijrah?bust=true",
+                           {cache: 'no-store'})
+      const json = await response.json();
+      setData(json);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  const updatePosition = () => {
+    console.log("Updating frame position")
+    setPosition(99999999999);
+  }
 
   useEffect(() => {
-    fetch("https://streaming.unrealasia.net/tvalhijrah")
-      .then((resp) => resp.json())
-      .then((json) => setData(json))
-      .catch((error) => console.error(error))
-
+    setLoading(true)
+    getLink()
     const subscription = AppState.addEventListener('change', nextAppState => {
       if (
         appState.current.match(/inactive|background/) &&
         nextAppState === 'active'
       ) {
-        fetch("https://streaming.unrealasia.net/tvalhijrah")
-          .then((resp) => resp.json())
-          .then((json) => setData(json))
-          .catch((error) => console.error(error)) 
-        setPosition(99999999999) 
         console.log('App has come to the foreground!');
       }
-
       appState.current = nextAppState;
       setAppStateVisible(appState.current);
       console.log('AppState', appState.current);
+      updatePosition();
     });
   }, []);
 
   return (
     <ThemedView>
-       <Video
-         // Can be a URL or a local file.
-         source={{uri: data.video}}
-         // Store reference  
-         ref={videoRef}
-         style={styles.backgroundVideo}
-         controls={true}
-         onPlaybackStateChanged={() => setPosition(99999999999)}
-         onPlaybackRateChange={() => setPosition(99999999999)}
-         startPosition={position}
-       />
+       {!isLoading && 
+         <Video
+           // Can be a URL or a local file.
+           source={{uri: data.video}}
+           // Store reference  
+           ref={videoRef}
+           style={styles.backgroundVideo}
+           controls={true}
+           onPlaybackStateChanged={() => setPosition(99999999999)}
+           onPlaybackRateChange={() => setPosition(99999999999)}
+           startPosition={position}
+         />
+       }
     </ThemedView>
   )
 }
